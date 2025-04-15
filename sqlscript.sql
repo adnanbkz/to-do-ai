e5e21e2c67c7305d899542d8fb6786c76251b704
Adnan Boukhzar <adnaneeuu@gmail.com>
2025-04-15 10:29:59 +0200

    first commit

--- sqlscript.sql
+++ sqlscript.sql
@@ -1,9 +1,11 @@
-
 -- create main database
 create database if not exists todolist;
+
 -- use the database for the next querys:
 use todolist;
+
 -- Create the main table
+
 Create table if not exists tareas (
 id int auto_increment primary key,
 titulo varchar(1000) not null,
@@ -11,6 +13,7 @@ descripcion varchar(5000),
 estado boolean default false,
 creado TIMESTAMP default CURRENT_TIMESTAMP
 );
+
 
 -- See if it works:
 start transaction;
